6cf8869bdcc85c4a3b3e867bb33a613c4356e796
pb_mohitjain <mohitjain@policybazaar.com>
2020-12-02 01:18:29 +0530

    change for db

--- dbconnection.php
+++ dbconnection.php
@@ -1,6 +1,6 @@
 <?php
 // Create connection
-$con=mysqli_connect("localhost","root","","onlinehospitalmanagementsystem");
+$con=mysqli_connect("klbcedmmqp7w17ik.cbetxkdyhwsb.us-east-1.rds.amazonaws.com","ja1jjwzl3da927c4","cly6xs20orqad776","hzfrszw32fvsfza6");
 
 // Check connection
 if (mysqli_connect_errno($con))
